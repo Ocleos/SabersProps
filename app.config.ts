75ed235eaa4589c26e1dca59d6e46214838a3a9b
Ocleos <ocleos@gmail.com>
2023-10-27 04:52:23 +0200

    chore(app): update to v0.0.9

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'SabersProps',
   slug: 'SabersProps',
-  version: '0.0.8',
+  version: '0.0.9',
   scheme: 'sabersprops',
   jsEngine: 'hermes',
   orientation: 'portrait',

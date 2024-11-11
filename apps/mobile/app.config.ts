0cf9d8e4123b3a07878e1269ef031e5950b44fcb
Ocleos <ocleos@gmail.com>
2024-11-11 02:04:19 +0100

    chore(app): update to v1.8.0

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'SabersProps',
   slug: 'SabersProps',
-  version: '1.7.0',
+  version: '1.8.0',
   owner: 'ocleos',
   scheme: 'sabersprops',
   jsEngine: 'hermes',
